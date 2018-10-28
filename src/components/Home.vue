7ab944584627e97197b45fc75cecd1ae4e6a6f77
Ru Chern Chong <me@ruchern.com>
2018-10-28 17:05:45 +0800

    Refactor component names
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,8 +8,8 @@
 
             <h2 class="subtitle has-text-weight-bold">Limit: {{ items.limit }}%</h2>
 
-            <progress-bar :id="index" :bar="bar" :limit="items.limit" :key="index"
-                          v-for="(bar, index) in items.bars"></progress-bar>
+            <ProgressBar :id="index" :bar="bar" :limit="items.limit" :key="index"
+                         v-for="(bar, index) in items.bars"></ProgressBar>
 
             <div class="select is-medium is-rounded">
               <select name="selectProgressBar" id="selectProgressBar" @change="onChange">
@@ -22,55 +22,55 @@
         </div>
 
         <div class="buttons is-centered">
-          <progress-button :button="button" :key="index" v-for="(button, index) in items.buttons"
-                           @setValue="setValue"></progress-button>
+          <ProgressButton :button="button" :key="index" v-for="(button, index) in items.buttons"
+                          @setValue="setValue"></ProgressButton>
         </div>
       </div>
     </div>
 
     <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
-      <loading-screen v-if="showLoader"></loading-screen>
+      <LoadingScreen v-if="showLoader"></LoadingScreen>
     </transition>
   </section>
 </template>
 
 <script>
-  import axios from 'axios'
-  import LoadingScreen from '@/components/LoadingScreen'
-  import ProgressBar from '@/components/ProgressBar'
-  import ProgressButton from '@/components/ProgressButton'
+import axios from 'axios'
+import LoadingScreen from '@/components/LoadingScreen'
+import ProgressBar from '@/components/ProgressBar'
+import ProgressButton from '@/components/ProgressButton'
 
-  export default {
-    name: 'Home',
-    components: { LoadingScreen, ProgressBar, ProgressButton },
-    data () {
-      return {
-        showLoader: true,
-        items: {},
-        selectedProgressBar: 0
-      }
-    },
-    mounted () {
-      this.getData().then(() => {
-        this.showLoader = false
+export default {
+  name: 'Home',
+  components: { LoadingScreen, ProgressBar, ProgressButton },
+  data () {
+    return {
+      showLoader: true,
+      items: {},
+      selectedProgressBar: 0
+    }
+  },
+  mounted () {
+    this.getData().then(() => {
+      this.showLoader = false
+    })
+  },
+  methods: {
+    getData () {
+      return axios.get('https://pb-api.herokuapp.com/bars').then(response => {
+        this.items = response.data
       })
     },
-    methods: {
-      getData () {
-        return axios.get('https://pb-api.herokuapp.com/bars').then(response => {
-          this.items = response.data
-        })
-      },
-      onChange (ev) {
-        this.selectedProgressBar = parseInt(ev.target.value)
-      },
-      setValue (value) {
-        let newValue = this.items.bars[this.selectedProgressBar] + value
+    onChange (ev) {
+      this.selectedProgressBar = parseInt(ev.target.value)
+    },
+    setValue (value) {
+      let newValue = this.items.bars[this.selectedProgressBar] + value
 
-        if (newValue <= 0) newValue = 0
+      if (newValue <= 0) newValue = 0
 
-        this.$set(this.items.bars, this.selectedProgressBar, newValue)
-      }
+      this.$set(this.items.bars, this.selectedProgressBar, newValue)
     }
   }
+}
 </script>
